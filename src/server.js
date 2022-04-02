26b5945d8d3e091fa6f99a082626801501e54216
Brady Camp <bradyjcamp@gmail.com>
2022-04-02 11:33:20 -0400

    all tests passing

--- src/server.js
+++ src/server.js
@@ -5,10 +5,14 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
-// Esoteric Resources
+
 const errorHandler = require('./error-handlers/500.js');
-const notFound = require('./error-handlers/404.js');
+const notFoundHandler = require('./error-handlers/404.js');
+const logger = require('./middleware/logger.js');
+const v1Routes = require('./routes/v1.js');
+const v2Routes = require('./routes/v2.js');
 const authRoutes = require('./auth/routes.js');
+
 
 // Prepare the express app
 const app = express();
@@ -20,18 +24,21 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(logger);
+
 // Routes
+app.use('/api/v1', v1Routes);
+app.use('/api/v2', v2Routes);
 app.use(authRoutes);
 
 // Catchalls
-app.use(notFound);
+app.use('*', notFoundHandler);
 app.use(errorHandler);
 
 module.exports = {
   server: app,
-  start: (port) => {
-    app.listen(port, () => {
-      console.log(`Server Up on ${port}`);
-    });
+  start: port => {
+    if (!port) { throw new Error('Missing Port'); }
+    app.listen(port, () => console.log(`Listening on ${port}`));
   },
 };
